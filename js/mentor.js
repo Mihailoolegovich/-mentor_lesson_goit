ccee9b272f270e446111042291adf4658bc47a52
Mihailoolegovich <mihailoolegovich@gmail.com>
2021-11-29 00:31:45 +0200

    ver 5

--- js/mentor.js
+++ js/mentor.js
@@ -8,22 +8,31 @@ rootEl.append(document.createElement("div"), document.createElement("div"));
 rootEl.firstElementChild.className = "firstDiv";
 rootEl.lastElementChild.className = "lastDiv";
 
-const rootStartDiv = document.querySelector(".firstDiv");
-const rootEndDiv = document.querySelector(".lastDiv");
+const refs = {
+  rootStartDiv: document.querySelector(".firstDiv"),
+  rootEndDiv: document.querySelector(".lastDiv"),
+};
 
-rootStartDiv.append(document.createElement("h2"));
-rootStartDiv.append(document.createElement("ul"));
-rootStartDiv.append(document.createElement("button"));
-
+refs.rootStartDiv.append(document.createElement("h2"));
 document.querySelector("h2").textContent = "My books";
+
+refs.rootStartDiv.append(document.createElement("ul"));
+
+refs.rootStartDiv.append(document.createElement("button"));
 document.querySelector("button").textContent = "Add";
 
 const rootUl = document.querySelector("ul");
 
-localStorage.setItem("books", JSON.stringify(bokLinc));
+if (!localStorage.getItem("books")) {
+  localStorage.setItem("books", JSON.stringify(bokLinc));
+}
 
 renderList();
 
+//
+// ......FUNCTION......
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//
 function renderList() {
   const books = JSON.parse(localStorage.getItem("books"));
 
@@ -40,30 +49,60 @@ function renderList() {
 
   rootUl.insertAdjacentHTML("afterbegin", bookLisMarcap);
 
-  const rootP = document.querySelectorAll(".title");
+  const aboutBook = document.querySelector(".about__book");
+  const rootParam = document.querySelectorAll(".title");
 
-  rootP.forEach((item) => item.addEventListener("click", renderPreview));
+  rootParam.forEach((item) => item.addEventListener("click", renderPreview));
 
-  const rootBtnDel = document.querySelector("#delete");
-  rootBtnDel.addEventListener("click", bookDelete);
+  const rootBtnDel = document.querySelectorAll("#delete");
+  rootBtnDel.forEach((item) => item.addEventListener("click", bookDelete));
 
   const rootBtnEd = document.querySelector("#edit");
   rootBtnEd.addEventListener("click", editDelete);
 }
-
+//
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//
 function renderPreview(evt) {
   const books = JSON.parse(localStorage.getItem("books"));
 
   const book = books.find(({ title }) => {
     return title === evt.currentTarget.textContent;
   });
-  console.log("book", book);
-}
 
-function bookDelete() {
-  console.log(2);
-}
+  refs.rootEndDiv.innerHTML = "";
+  const iformList = `<h3>${book.title}</h3>
+  <p>${book.author}</p>
+  <img src="${book.img}">
+  <p>${book.plot}</p>`;
 
-function editDelete() {
-  console.log(3);
+  refs.rootEndDiv.innerHTML = iformList;
 }
+//
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//
+function bookDelete(evt) {
+  const books = JSON.parse(localStorage.getItem("books"));
+
+  const parentTitle = evt.currentTarget.parentNode.querySelector(".title");
+  const lastDivTitle = refs.rootEndDiv.querySelector("h3");
+
+  const fg = books.filter(({ title }) => title !== parentTitle.textContent);
+
+  localStorage.setItem("books", JSON.stringify(fg));
+
+  if (localStorage.getItem("books").length === 2) {
+    localStorage.removeItem("books");
+  }
+  if (lastDivTitle) {
+    if (lastDivTitle.textContent === parentTitle.textContent) {
+      refs.rootEndDiv.innerHTML = "";
+    }
+  }
+  evt.currentTarget.parentNode.remove();
+}
+//
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//
+
+function editDelete() {}
